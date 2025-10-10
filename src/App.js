20aac92091991053c81acf07f3a23489b60131a9
shruteeiyer1 <shrutee.iyer1@mitwpu.edu.in>
2025-10-10 11:47:22 +0530

    FSD ASSIGNMENT 3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousel from "./Carousel";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>My Photo Carousel</h1>
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Carousel.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Carousel.css";
+
+const Carousel = () => {
+  const images = [
+    'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9YYh5Fk1u9VsWWr1MhkyQeOzeNbtnnMO96g&s',
+    'https://images.pexels.com/photos/1054655/pexels-photo-1054655.jpeg?cs=srgb&dl=pexels-hsapir-1054655.jpg&fm=jpg'
+  ];
+
+  const [current, setCurrent] = useState(0);
+
+  const nextSlide = () => {
+    setCurrent((prev) => (prev + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrent((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="carousel">
+      <button onClick={prevSlide} className="nav-btn">❮</button>
+      <img src={images[current]} alt="slide" className="carousel-img" />
+      <button onClick={nextSlide} className="nav-btn">❯</button>
+    </div>
+  );
+};
+
+export default Carousel;
